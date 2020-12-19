0176f898b1e63db63237bdf2649d36b15b10e091
sgtkaykay <60122578+sgtkaykay@users.noreply.github.com>
2020-12-19 23:27:12 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,47 @@
+<div class="about">
+  <div class="lefteye"></div>
+  <div class="righteye"></div>
+  <img class="hi" src="home/hi.svg" alt="hi">
+  <div class="aboutmecont">
+    <h1 class="aboutme">I'm Christian Faustinorio!</h1>
+    <p>
+      Welecome to my portfolio.<br/><br/>
+      I'm an IT student in University of Saint La Salle.<br/>
+      I aspire to be a decent web designer/developer.<br/>
+      Thanks for looking at my portfolio!
+    </p>
+  </div>
+  <div class="circle3"></div>
+  <div class="circle1"></div>
+  <div class="circle2"></div>
+  <div class="circle4"></div>
+  <div class="circle5"></div>
+  <div class="circle6"></div>
+  <div class="circle7"></div>
+  <div class="circle8"></div>
+  <div class="circle9"></div>
+  <div class="circle10"></div>
+</div>
+<div class="buttonsCont blue">
+  <h1>CSS Github</h1>
+  <a href="https://github.com/sgtkaykay/portfolio-cle/tree/main/CSSWorks"><button class="button"><span>CSS </span></button></a>
+</div>
+<div class="buttonsCont orange">
+  <h1>SVG Github</h1>
+  <a href="https://github.com/sgtkaykay/portfolio-cle/tree/main/clesvg"><button class="button"><span>SVG </span></button></a>
+</div>
+<div class="buttonsCont red">
+  <h1>Canvas Github</h1>
+  <a href="https://github.com/sgtkaykay/portfolio-cle/tree/main/Canvas"><button class="button"><span>Canvas </span></button></a>
+</div>
+<div class="buttonsCont green">
+  <h1>Social Medias</h1>
+  <div class="half-colored">
+    <ul>
+      <a href="https://web.facebook.com/kreyo94"><li><i class="fa fa-facebook" aria-hidden="true"></i></li></a>
+      <a href="https://twitter.com/Cexotics77"><li><i class="fa fa-twitter" aria-hidden="true"></i></li></a>
+      <a href="https://www.instagram.com/oh_christian94/"><li><i class="fa fa-instagram" aria-hidden="true"></i></li></a>
+      <a href="https://github.com/sgtkaykay"><li><i class="fa fa-github" aria-hidden="true"></i></li></a>
+    </ul>
+  </div>
+</div>
